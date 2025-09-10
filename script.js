62d717a482a9323fee49f85c91d452d6bd56f34e
an0u5h3h <hussainanousheh@gmail.com>
2025-09-10 20:03:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+class Movie{
+    constructor(title, director, year){
+        this.title=title
+        this.director=director
+        this.year=year
+    }
+}
+const defaultMovies=[
+    {title:'Jurassic Park', director:'John Doe', year:'1990'},
+    {title:'The Dead Pool', director:'Mathew Albison', year:'2014'},
+    {title:'The Avengers', director:'Joss Whedon', year:'2012'},
+]
+class Ui{
+    static addMoviesToTheList=(movie) => {
+        const list=document.getElementById('movie-list');
+        const row=document.createElement('tr');
+        row.innerHTML=
+        `<td>${movie.title}</td>
+        <td>${movie.director}</td>
+        <td>${movie.year}</td>`
+        list.appendChild(row)
+    }
+    static displayMovies=() => {
+        defaultMovies.forEach(movie => Ui.addMoviesToTheList(movie)) 
+}
+}
+Ui.displayMovies()
+document.querySelector('#dataform').addEventListener('addMovie',addMovie)
